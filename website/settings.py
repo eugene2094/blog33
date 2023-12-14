20ef49a5cc56cbf015f7a886257f9b08f21f398a
eugene2094 <70202369+eugene2094@users.noreply.github.com>
2023-12-14 21:00:20 +0300

    Update settings.py

--- website/settings.py
+++ website/settings.py
@@ -129,5 +129,5 @@ LOGOUT_URL = 'blog_logout'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
 MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+# STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
